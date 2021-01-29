e819c03c35a5fee44bf435accecf985cb9c08fce
anduolaya04 <36541393+anduolaya04@users.noreply.github.com>
2021-01-29 00:02:20 -0500

    Proof enrollement

---
+++ src/main/java/com/apes/prueba/Carrito.java
@@ -0,0 +1,97 @@
+package com.apes.prueba;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+
+enum Categorias{
+
+    CHARCUTERIA,
+    PANADERIA,
+    ASEO,
+    ELECTRODOMESTICOS,
+    MASCOTAS
+};
+
+public class Carrito {
+
+    static List<Producto> productos;
+
+
+    final String cliente ;
+
+
+    static int total = 0;
+
+
+    public Carrito() {
+
+        cliente = "hola";
+        productos = new ArrayList<>();
+    }
+
+    static void llenarCarrito(Producto producto ) {
+
+        productos.add( producto );
+    }
+
+
+    static class Producto {
+
+        int precio;
+
+        String nombre;
+
+        int cantidad;
+
+        Categorias categoria;
+
+        public void setPrecio(int precio) {
+            this.precio = precio;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void setCantidad(int cantidad) {
+            this.cantidad = cantidad;
+        }
+
+        public void setCategoria(Categorias categoria) {
+            this.categoria = categoria;
+        }
+
+        int precioProductos(){
+
+            return cantidad * precio;
+        }
+
+        @Override
+        public String toString() {
+            return "Producto{" +
+                    "precio=" + precio +
+                    ", nombre='" + nombre + '\'' +
+                    ", cantidad=" + cantidad +
+                    ", categoria=" + categoria +
+                    '}';
+        }
+    }
+
+    static List<Producto> tenerCarrito(){
+
+        return Collections.unmodifiableList( productos );
+    }
+
+    static int calculaPrecioCarrito(){
+
+        for (Producto producto: productos) {
+            total = producto.precioProductos() + total;
+        }
+
+        return total;
+    }
+}
---
+++ src/main/java/com/apes/prueba/main.java
@@ -0,0 +1,31 @@
+package com.apes.prueba;
+
+public class main {
+
+    public static void main(String[] args)
+    {
+        Carrito.Producto productoP = new Carrito.Producto();
+
+        productoP.setCantidad( 6 );
+        productoP.setNombre( "Jabon" );
+        productoP.setPrecio( 2000 );
+
+        Carrito.llenarCarrito(productoP);
+
+        llenarCarritoPapas();
+
+        Carrito.tenerCarrito().stream().forEach( System.out::println );
+        System.out.println( "Carrito.precioTotal = " + Carrito.calculaPrecioCarrito() );
+    }
+
+    static void llenarCarritoPapas(){
+
+        Carrito.Producto productoP = new Carrito.Producto();
+
+        productoP.setCantidad( 25 );
+        productoP.setNombre( "papas" );
+        productoP.setPrecio( 1700 );
+
+        Carrito.llenarCarrito( productoP );
+    }
+}
